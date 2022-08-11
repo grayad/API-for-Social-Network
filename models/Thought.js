4720fa55ba27a2980f71f70c025b267a7b64f281
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-08-11 17:23:18 -0400

    add JSON object to model to use virtuals and getters

--- models/Thought.js
+++ models/Thought.js
@@ -1,28 +1,37 @@
 const { Schema, model } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
-const ThoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    validate: [
-      ({ length }) => length <= 280,
-      "Thought should be less than 280 characters.",
-    ],
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      validate: [
+        ({ length }) => length <= 280,
+        "Thought should be less than 280 characters.",
+      ],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: {
+      // TODO: add reaction schema?
+    },
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) => dateFormat(createdAtVal),
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: {
-    // TODO: add reaction schema?
-  },
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 const Thought = model("Thought", ThoughtSchema);
 
