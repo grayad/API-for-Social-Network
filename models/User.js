b75ed540e45f2d80bc1ae4038fb475dcd0fe8a77
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-08-10 20:26:17 -0400

    create virtual for friendCount

--- models/User.js
+++ models/User.js
@@ -1,37 +1,50 @@
 const { Schema, model } = require("mongoose");
 
 // user model
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    trim: true,
-    unique: true,
-    required: "Username is required",
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: "Email is required",
-    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
-  },
-  userCreated: {
-    type: Date,
-    default: Date.now,
-  },
-  //Array of _id values referencing the Thought model
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Thought",
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: "Username is required",
     },
-  ],
-  // Array of _id values referencing the User model (self-reference)
-  friends: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
+    email: {
+      type: String,
+      unique: true,
+      required: "Email is required",
+      match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
     },
-  ],
+    userCreated: {
+      type: Date,
+      default: Date.now,
+    },
+    //Array of _id values referencing the Thought model
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    // Array of _id values referencing the User model (self-reference)
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// virtual called friendCount that retrieves the length of the user's friends array field on query.
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
 const User = model("User", UserSchema);
