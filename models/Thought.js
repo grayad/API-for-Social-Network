4a0b481d4cbc9f0d414b17932024030b7b91398b
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-08-13 22:41:00 -0400

    create reaction schema and link to thoughts schema as subdocument array

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,37 @@
 const { Schema, model } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
+const ReactionSchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent thought _id
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      validate: [
+        ({ length }) => length <= 280,
+        "Reaction should be less than 280 characters.",
+      ],
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
@@ -20,12 +51,7 @@ const ThoughtSchema = new Schema(
       type: String,
       required: true,
     },
-    reactions: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Reaction",
-      },
-    ],
+    reactions: [ReactionSchema],
   },
   {
     toJSON: {
