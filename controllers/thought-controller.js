ae52628d42862ca2aeef6ee70e91a3b3f644fba8
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-08-17 17:55:23 -0400

    fix delete thought function

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -36,7 +36,6 @@ const thoughtController = {
     console.log(body);
     Thought.create(body)
       .then(({ _id }) => {
-        console.log("USER ID", _id);
         return User.findOneAndUpdate(
           { _id: params.userId },
           //   use $push method to add the thoughts's id to the specific user
@@ -67,26 +66,26 @@ const thoughtController = {
   },
   // delete Thought
   deleteThought({ params }, res) {
-    Thought.findOneAndDelete({ _id: params.id })
+    Thought.findOneAndDelete({ _id: params.thoughtId })
       .then((dbThoughtData) => {
         if (!dbThoughtData) {
           res.status(404).json({ message: "No thought found with this id!" });
         }
-        // use thought data to identify and remove it from the associated user
-        return User.findOneAndUpdate(
-          { _id: params.userId },
-          //   $pull is the mongo operation to remove the thought
-          { $pull: { thoughts: params.thoughtId } },
-          { new: true }
-        );
+        // // use thought data to identify and remove it from the associated user
+        // return User.findOneAndUpdate(
+        //   { _id: params.userId },
+        //   //   $pull is the mongo operation to remove the thought
+        //   { $pull: { thoughts: params.thoughtId } },
+        //   { new: true }
+        // );
+        res.json(dbThoughtData);
       })
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          res.status(404).json({ message: "No user found with this id!" });
-          return;
-        }
-        res.json(dbUserData);
-      })
+      // .then((dbUserData) => {
+      //   if (!dbUserData) {
+      //     res.status(404).json({ message: "No user found with this id!" });
+      //   }
+      //   res.json(dbUserData);
+      // })
       .catch((err) => res.status(400).json(err));
   },
 };
