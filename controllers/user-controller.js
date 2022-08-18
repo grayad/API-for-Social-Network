cca20520fc6b4bbbfa078cbabe50f369e6ce77f0
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-08-18 19:25:48 -0400

    fix remove friend function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -92,7 +92,7 @@ const userController = {
   removeFriend({ params }, res) {
     User.findOneAndUpdate(
       { _id: params.userId },
-      { $pull: { friends: { friendId: params.friendId } } },
+      { $pull: { friends: params.friendId } },
       { new: true }
     )
       .then((dbUserData) => res.json(dbUserData))
